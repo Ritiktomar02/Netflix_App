b219d2f72ab9aaef4b48877b5d68d8c6d4b4aaaa
Ritik Tomar <ritiktomar@Ritiks-MacBook-Air.local>
2025-01-29 12:37:25 +0530

    new

--- server/index.js
+++ server/index.js
@@ -1,15 +1,12 @@
-// Step-1
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
-const PORT=process.env.PORT || 4000;
-
+const PORT = process.env.PORT || 4000;
 
 require("./utils/database").databaseConnection();
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,9 +18,13 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+// Define a route for the root path
+app.get("/", (req, res) => {
+    res.send("Welcome to the Netflix App API!");
+});
+
 const userRoute = require("./routes/userRoute");
 app.use("/api/v1", userRoute);
-
 
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}`);
